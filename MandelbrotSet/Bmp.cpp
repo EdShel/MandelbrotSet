ddaf5ad972387a1be5f5b2e82f9029c7791df259
EdShel <eduardchelemetjev@gmail.com>
2020-11-24 10:34:00 +0200

    Add project files.

---
+++ MandelbrotSet/Bmp.cpp
@@ -0,0 +1,42 @@
+#include <Windows.h>
+
+#pragma pack(push, 1)
+struct BitmapFileHeader
+{
+	INT16 type;
+	INT32 fileSize;
+	INT16 reserved1;
+	INT16 reserved2;
+	INT32 offsetToData;
+};
+#pragma pack(pop)
+
+
+#pragma pack(push, 1)
+struct BitmapInfoHeader
+{
+	INT32 headerSize;
+	INT32 imageWidth;
+	INT32 imageHeight;
+	INT16 planesCount;
+	INT16 bitsPerPixel;
+	INT32 compression;
+	INT32 imageDataSize;
+	INT32 pxlPerMeterX;
+	INT32 pxlPerMeterY;
+	INT32 colorsCount;
+	INT32 colorsImportantCount;
+};
+#pragma pack(pop)
+
+union RgbQuad
+{
+	struct
+	{
+		BYTE rgbBlue;
+		BYTE rgbGreen;
+		BYTE rgbRed;
+		BYTE rgbReserved;
+	} RgbStruct;
+	INT32 RgbColor;
+};
---
+++ MandelbrotSet/MandelbrotSet.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include "Bmp.cpp"
+#include <tchar.h>
+
+int main()
+{
+	HANDLE file = CreateFile(
+		_T("C:\\Users\\Admin\\Desktop\\test.bmp"),
+		GENERIC_WRITE,
+		FILE_SHARE_READ,
+		NULL,
+		CREATE_ALWAYS,
+		0,
+		NULL);
+	if (file != INVALID_HANDLE_VALUE)
+	{
+		BitmapFileHeader fileHeader;
+		fileHeader.type = 19778;
+		fileHeader.reserved1 = 0;
+		fileHeader.reserved2 = 0;
+
+		BitmapInfoHeader infoHeader;
+		infoHeader.headerSize = sizeof(BitmapInfoHeader);
+		infoHeader.imageWidth = 4;
+		infoHeader.imageHeight = 4;
+		infoHeader.planesCount = 1;
+		infoHeader.bitsPerPixel = 8;
+		infoHeader.compression = 0;
+		infoHeader.imageDataSize = 0;
+		infoHeader.pxlPerMeterX = 0;
+		infoHeader.pxlPerMeterY = 0;
+		infoHeader.colorsCount = 2; //0;
+		infoHeader.colorsImportantCount = 0;
+
+		RgbQuad yellow; 
+		yellow.RgbColor = 0xe8eb34;
+		RgbQuad blue;
+		blue.RgbColor = 0x0000FF;
+		UINT colorTableSize = 2;//1 << infoHeader.bitsPerPixel;
+		RgbQuad* colorTable = new RgbQuad[colorTableSize];
+		colorTable[0] = yellow;
+		colorTable[1] = blue;
+
+		BYTE pixelData[16] = {
+			0, 0, 0, 0,
+			0, 0, 0, 0,
+			1, 1, 1, 1,
+			1, 1, 1, 1
+		};
+
+		fileHeader.offsetToData = sizeof(fileHeader)
+			+ sizeof(infoHeader)
+			+ sizeof(RgbQuad) * (colorTableSize);
+		fileHeader.fileSize = fileHeader.offsetToData + sizeof(pixelData);
+
+		WriteFile(file, &fileHeader, sizeof(fileHeader), NULL, NULL);
+		WriteFile(file, &infoHeader, sizeof(infoHeader), NULL, NULL);
+		WriteFile(file, colorTable, sizeof(RgbQuad) * colorTableSize, NULL, NULL);
+		WriteFile(file, pixelData, sizeof(pixelData), NULL, NULL);
+
+		CloseHandle(file);
+		delete[] colorTable;
+	}
+}
